b7fcdd0378d85d6075c5ef8426fc79cebe966ad8
Ty Everett <ty@tyweb.us>
2022-08-28 12:17:30 -0700

    Update defaults.js

--- src/defaults.js
+++ src/defaults.js
@@ -1,9 +1,5 @@
 module.exports = {
   CONFIG: {
-    nanostoreURL: window.location.host.startsWith('localhost')
-      ? 'http://localhost:3104'
-      : process.env.REACT_APP_IS_STAGING
-        ? 'https://staging-nanostore.babbage.systems'
-        : 'https://nanostore.babbage.systems'
+    nanostoreURL: 'https://nanostore.babbage.systems'
   }
 }
