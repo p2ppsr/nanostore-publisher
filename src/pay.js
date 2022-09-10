cf7aaf8d233a17e018850a1ada71b2ea474022b0
Ty Everett <ty@tyweb.us>
2022-09-09 22:33:34 -0700

    Use new babbage-bsv

--- src/pay.js
+++ src/pay.js
@@ -1,4 +1,4 @@
-const bsv = require('bsv')
+const bsv = require('babbage-bsv')
 const { createAction, getPublicKey } = require('@babbage/sdk')
 const createSignedRequest = require('./utils/createSignedRequest')
 const { CONFIG } = require('./defaults')
